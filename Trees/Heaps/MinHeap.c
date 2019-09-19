12011eef125b268e4a7e9aaeaab41d68c21abfff
AlokikPathak <alokik2009@gmail.com>
2019-09-19 17:22:27 +0530

    implement min-heap and refactor

--- Trees/Heaps/MinHeap.c
+++ Trees/Heaps/MinHeap.c
@@ -45,60 +45,60 @@ void printArray(int arr[],  int size)
 }
 
 /**
- * Name: maxHeapify()
- * Purpose: Max-heapify the given root of sub-tree as array 
+ * Name: minHeapify()
+ * Purpose: Min-heapify the given root of sub-tree as array 
  * @param: int* arr, pointer to heap array
  * @param: unsigned int size, stores size of heap array
- * @param: unsigned int nodeIndex, contains index of root node of sub-tree needed to be max-heap
+ * @param: unsigned int nodeIndex, contains index of root node of sub-tree needed to be mix-heap
  * @return: void
  * @complexity: O(logN) i.e. logrithimic time
 */
-void maxHeapify(int *arr, int size, int nodeIndex)
+void minHeapify(int *arr, int size, int nodeIndex)
 {
 
 	int left = (2 * nodeIndex) + 1;  // left child Node index( since array is from 0 so +1)
 	int right = (2 * nodeIndex) + 2; // right child Node index
-	int largest = nodeIndex;		 // initialize as root node
+	int smallest = nodeIndex;		 // initialize as root node
 
-	if (left < size && arr[left] > arr[largest])
-		largest = left;
-	if (right < size && arr[right] > arr[largest])
-		largest = right;
+	if (left < size && arr[left] < arr[smallest])
+		smallest = left;
+	if (right < size && arr[right] < arr[smallest])
+		smallest = right;
 
 	// largest in not root Node index(i.e. modified)
-	if (largest != nodeIndex)
+	if (smallest != nodeIndex)
 	{
-		// swapping Root Node with child having largest value & build Max Heap Sub-tree
-		swapTwoNum(&arr[nodeIndex], &arr[largest]);
-		// recursively MaxHeapify the new subtree formed after swap
-		maxHeapify(arr, size, largest);
+		// swapping Root Node with child having smallest(min) value & build Min Heap Sub-tree
+		swapTwoNum(&arr[nodeIndex], &arr[smallest]);
+		// recursively MinHeapify the new subtree formed after swap
+		minHeapify(arr, size, smallest);
 	}
 }
 
 /**
- * Name: buildMaxHeap()
- * Purpose: Build Max heap out of an array visualized as Heap
+ * Name: buildMinHeap()
+ * Purpose: Build Min heap out of an array visualized as Heap
  * @param: int arr[]
  * @param: int n, size of array
  * @return: void
- * @complexity : O(N) (max_heapify complexity: O(logN) and total n/2 steps so N linear)
+ * @complexity : O(N) (min_heapify complexity: O(logN) and total n/2 steps so N constant)
  * If array is visualized as nearly Complete Binary Tree, all the elements after n/2 to n are leaves
- * In heaps, the leaves are automatically a Max Heap, since they don't have child
+ * In heaps, the leaves are automatically a Min Heap, since they don't have child
  * 
 */
-void buildMaxHeap(int *arr, int n)
+void buildMinHeap(int *arr, int n)
 {
 	int i;
 	for (i = (n / 2) - 1; i >= 0; i--)
-		maxHeapify(arr, n, i);
+		minHeapify(arr, n, i);
 }
 
 /**
  * Name: main()
  * Pupose: Starts execution of program
  * @return: int
- * ex. n = 10, arr[n]= {14, 16, 10, 9, 7, 8, 3, 2, 4, 1}
- * Max_Heap: {16, 14, 10, 9, 7, 8, 3, 2, 4, 1}
+ * ex. n = 6, arr[n]= {18, 17, 9, 2, 6, -10}
+ * Min_Heap: {18, 17, 9, 2, 6, -10}
 */
 int main()
 {
@@ -107,8 +107,8 @@ int main()
 	scanf("%d", &n);
 	int arr[n];
 	takeArrayInput(arr, n);
-	buildMaxHeap(arr, n);
-	printf("Max Heap: \n");
+	buildMinHeap(arr, n);
+	printf("Min Heap: \n");
 	printArray(arr, n);
 
 	return 0;
