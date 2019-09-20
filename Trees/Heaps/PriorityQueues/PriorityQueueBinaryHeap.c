cc016e8c5eb449525cc26a217d1f4a6c893eea3e
AlokikPathak <alokik2009@gmail.com>
2019-09-20 20:02:55 +0530

    implement priority queue initial commit

---
+++ Trees/Heaps/PriorityQueues/PriorityQueueBinaryHeap.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+/**
+ * Heaps are mainly used in build efficient Priority Queues which are used in 
+ * Prims Minimum Spanning Trees, Dijikistra's and all scheduling algorithms in OS
+*/
